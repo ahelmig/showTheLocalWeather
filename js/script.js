b3c7183469aeb14702f25b4880b52582b8afb465
Andrea McDonald <acm4m7@mail.missouri.edu>
2016-11-07 14:47:26 -0700

    Updated images and API changes
    - Added new images for obsolete pixabay images
    - Added handling for no city information provided with ipinfo API

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 var temp = '';
 
 function toCelsius(kelvin) {
-  return (kelvin-273);
+  return (kelvin - 273);
 }
 
 function toFahrenheit(kelvin) {
-  return Math.floor((((kelvin-273) * 1.8) + 32));
+  return Math.floor((((kelvin - 273) * 1.8) + 32));
 }
 
 function fetchWeather() {
   $.getJSON('http://ipinfo.io', function(data) {
+    console.log(data)
     var userCity = data.city;
+    if (userCity === ''){
+      $('#userLoc').html("Bad news, our robot can't figure out what city you're in. Silly robot.")
+      $('#changeTemp').addClass('hidden')
+      $('h1').addClass('hidden')
+    }
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&APPID=1d85bfdf5c6a1fbb8f41feeec23eee84&&callback=?', function(info) {
+      console.log(info)
+      if (info.message === "Error: Not found city"){
+       $('#userLoc').html("Bad news, the OpenWeather API doesn't have weather info on your city.");
+      }
+      else{
       $('#userLoc').html(info.name + ", " + info.sys.country);
+      $('#description').html(info.weather[0].main);
       var weatherDesc = info.weather[0].main;
       if (weatherDesc === 'Clouds') {
         $("body").css('background-image', 'url("https://pixabay.com/static/uploads/photo/2013/10/13/00/08/clouds-194840_960_720.jpg")');
       }
-      if (weatherDesc === 'Fog') {
+      if (weatherDesc === 'Haze') {
         $("body").css('background-image', 'url("https://pixabay.com/static/uploads/photo/2013/01/30/20/44/sunbeam-76825_960_720.jpg")');
       }
       if (weatherDesc === 'Rain') {
@@ -28,20 +40,20 @@ function fetchWeather() {
       }
       if (weatherDesc === 'Thunderstorm') {
         $("body").css('background-image', 'url("https://pixabay.com/static/uploads/photo/2014/06/26/22/02/lightning-378069_960_720.jpg")');
-      }
-      else if (weatherDesc === 'Clear') {
+      } else if (weatherDesc === 'Clear') {
         $("body").css('background-image', 'url("https://pixabay.com/static/uploads/photo/2014/10/03/16/53/refreshing-471950_960_720.jpg")');
       }
       temp = Math.floor(info.main.temp);
       if (info.sys.country !== 'US') {
         $('#changeTemp').append('Fahrenheit');
         $('#tempLetter').append('C°');
-        $('#temperature').html(toCelsius(temp));
+        $('#temperature').html(toCelsius(temp)).text();
       } else {
         $('#changeTemp').append('Celsius');
         $('#tempLetter').append('F°');
-        $('#temperature').html(toFahrenheit(temp));
+        $('#temperature').html(toFahrenheit(temp)).text();
       }
+     }
     });
   })
 }
@@ -50,13 +62,14 @@ $(document).ready(function() {
   fetchWeather();
   $("#changeTemp").on('click', function() {
     if ($(this).text() == 'Fahrenheit') {
-      $('#temperature').html(toFahrenheit(temp));
+      $('#temperature').html(toFahrenheit(temp)).text();
       $(this).text('Celsius');
       $('#tempLetter').text('F°');
     } else if ($(this).text() == 'Celsius') {
-      $('#temperature').html(toCelsius(temp));
+      $('#temperature').html(toCelsius(temp)).text();
       $(this).text('Fahrenheit');
       $('#tempLetter').text('C°');
     }
   })
 });
+
